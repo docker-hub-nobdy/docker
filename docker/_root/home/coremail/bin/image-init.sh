d079ced106357c4099f1da3b1ec7ecf968c8a733
nobdy <nobdy@163.com>
2018-11-20 22:46:56 +0800

    Update image-init.sh

--- docker/_root/home/coremail/bin/image-init.sh
+++ docker/_root/home/coremail/bin/image-init.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-sed -i 'N3aecho "1. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
+sed -i '3iecho "1. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
 
 sed -i -r 's/^([\t ]*)("\$@"[\t ]*)$/\1${DOCKER_OPTS} \2/' /usr/local/bin/dockerd-entrypoint.sh
 
-sed -i 'N14aecho "2. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
+sed -i '14iecho "2. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
 
-sed -i 'N24aecho "3. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
+sed -i '24iecho "3. $@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
 
-sed -i 'N24aecho "DOCKER_HOST: ${DOCKER_HOST}" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
+sed -i '24iecho "DOCKER_HOST: ${DOCKER_HOST}" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
 
 
 echo "/usr/local/bin/dockerd-entrypoint.sh patched" >> /patch_result
