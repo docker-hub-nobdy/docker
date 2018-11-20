f6dc043016cc34ef0abd54fd446e4851beb96417
nobdy <nobdy@163.com>
2018-11-20 22:09:03 +0800

    Update image-init.sh

--- docker/_root/home/coremail/bin/image-init.sh
+++ docker/_root/home/coremail/bin/image-init.sh
@@ -3,5 +3,6 @@
 
 sed -i -r 's/^([\t ]*)("\$@"[\t ]*)$/\1${DOCKER_OPTS} \2/' /usr/local/bin/dockerd-entrypoint.sh
 
-echo "/usr/local/bin/dockerd-entrypoint.sh patched" >> /patch_result
+sed -i 'N22aecho "$@" >> /patch_result' /usr/local/bin/dockerd-entrypoint.sh
 
+echo "/usr/local/bin/dockerd-entrypoint.sh patched" >> /patch_result
